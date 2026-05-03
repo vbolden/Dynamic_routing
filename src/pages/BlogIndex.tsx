7229b441868a7031b5a6ede922059f3a55485f65
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-03 17:24:56 -0400

    added UI rendering for posts to be added dynamically

--- src/pages/BlogIndex.tsx
+++ src/pages/BlogIndex.tsx
@@ -2,7 +2,17 @@ import { posts } from "../library/posts";
 
 function BlogIndex() {
     return (
-        
+        <div>
+            <h1>Blog Posts</h1>
+
+            <ul>
+                {posts.map((post) => (
+                    <li key={post.id} >
+                        {post.title}
+                    </li>
+                ))}
+            </ul>
+        </div>
     )
 }
 
