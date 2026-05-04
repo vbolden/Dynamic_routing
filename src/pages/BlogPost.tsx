780652a445c459052df1ce77c2d8f7d337bddb63
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-04 05:09:51 -0400

    added BlogPost function that renders post title and content

---
+++ src/pages/BlogPost.tsx
@@ -0,0 +1,22 @@
+import { useParams } from "react-router-dom";
+import { posts } from "../library/posts";
+
+function BlogPost() {
+    // DESTRUCTURE TO PULL SLUG VALUE FROM USEPARAMS OBJECT
+    const {slug} = useParams();
+
+    // RETURN POST WITH SLUG MATCHING URL PARAM
+    const post = posts.find(
+        (p) => p.slug === slug
+    );
+
+    return (
+        <div>
+            <h2>{post.title}</h2>
+
+            <p>{post.content}</p>
+        </div>
+    )
+}
+
+export default BlogPost;
