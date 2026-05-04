42f7e28810a7d2da4fbf7b015c8cdee58f4f6fd4
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-04 06:14:16 -0400

    imported useAuth

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,8 @@
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
 function Login() {
+    const { login } = useAuth();
     const navigate = useNavigate();
 
     function handleLogin() {
