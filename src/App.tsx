256d3f300864315a80e8534558d4fe7b59699220
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-04 06:21:52 -0400

    added imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,12 @@
 import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import ProtectedRoute from './components/ProtectedRoute'
+import BlogIndex from './pages/BlogIndex'
+import BlogPost from './pages/BlogPost'
+import Login from './pages/Login'
+import Admin from './pages/Admin'
+
 
 function App() {
 
