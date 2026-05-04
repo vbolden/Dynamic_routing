d5839d64033fbe96f3c429e07c7521ca32a91315
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-04 06:04:38 -0400

    added imports, function ProtectedRoute, exported function

---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,8 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+function ProtectedRoute({children}: {children: React.ReactNode}) {
+
+}
+
+export default ProtectedRoute;
