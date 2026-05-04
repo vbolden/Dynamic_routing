4e351733feffbbf96b7e41c023e47f61751882f6
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-04 05:24:05 -0400

    imported useNavigation, created function Login, exported function

---
+++ src/pages/Login.tsx
@@ -0,0 +1,11 @@
+import { useNavigate } from "react-router-dom";
+
+function Login() {
+    const navigate = useNavigate();
+
+    function handleLogin() {
+        
+    }
+}
+
+export default Login;
