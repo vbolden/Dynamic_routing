206a251891b49a71303bdfbb6f68f5b52508e7dd
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-04 06:08:17 -0400

    added conditional to send user to login in page if they are not authorized

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,8 +1,15 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
-function ProtectedRoute({children}: {children: React.ReactNode}) {
+function ProtectedRoute({ children }: { children: React.ReactNode }) {
+    const { isAuth } = useAuth();
 
+
+    if (!isAuth) {
+        return <Navigate to="/login" replace />; // IF NOT AUTH SEND TO LOGIN PAGE
+    }
+
+    return children;
 }
 
 export default ProtectedRoute;
