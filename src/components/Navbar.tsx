cd4873d10f136e25fcedd49d67ae36d029834dab
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-04 05:52:14 -0400

    added UI rendering for page navigation links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,24 @@ import { useAuth } from "../context/AuthContext";
 import { Link } from "react-router-dom";
 
 function Navbar() {
+    const { isAuth, logout } = useAuth();
 
+    return (
+        <nav className="navbar">
+            <Link to="/blog" >Blog</Link>
+
+            {isAuth ? (
+                <>
+                    <Link to="/admin">Admin</Link>
+                    <button onClick={logout} >
+                        Log Out
+                    </button>
+                </>
+            ) : (
+                <Link to="/login" >Log In</Link>
+            )}
+        </nav>
+    );
 }
 
 export default Navbar;
