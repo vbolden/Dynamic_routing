8c6737a4f2936587ae54c7ab997136d6cc667d1d
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-04 06:18:58 -0400

    added UI for Admin page

---
+++ src/pages/Admin.tsx
@@ -0,0 +1,13 @@
+function Admin() {
+    return (
+        <div>
+            <h2>Admin Dashboard</h2>
+
+            <p>
+                Welcome to the Admin Dashboard.
+            </p>
+        </div>
+    )
+}
+
+export default Admin;
