91ee21a5f74aa9e61d0a7fba700d43a106c3f61b
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-04 06:29:38 -0400

    added pages and components rendering

--- src/App.tsx
+++ src/App.tsx
@@ -11,10 +11,40 @@ import Admin from './pages/Admin'
 function App() {
 
   return (
-    <>
-      
-    </>
-  )
+    <BrowserRouter>
+      <Navbar />
+
+      <div>
+        <Routes>
+          <Route 
+          path="/"
+          element={<Navigate to="/blog" />}
+          />
+          
+          <Route 
+          path="/blog"
+          element={<BlogIndex />}
+          />
+          <Route 
+          path="/blog/:slug"
+          element={<BlogPost />}
+          />
+          <Route 
+          path="/login"
+          element={<Login />}
+          />
+          <Route 
+          path="/admin"
+          element={
+            <ProtectedRoute>
+              <Admin />
+            </ProtectedRoute>
+          }
+          />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App
